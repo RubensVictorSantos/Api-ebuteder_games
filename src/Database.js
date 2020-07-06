69d86abc416558249ba38e29ffc8be2b164ef493
Rubens Victor <rubensvictorsantos@gmail.com>
2020-07-06 10:55:14 -0300

    Sla

--- src/Database.js
+++ src/Database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-    const sequelize = new Sequelize("db_ebutder_games","root","bcd127",{
+    const sequelize = new Sequelize("db_ebutder","root","Ladera*610892",{
         host:"localhost",
         dialect:"mysql",
         define:{
